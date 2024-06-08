40b974f315116dc05414c0d5bbbfe355ca720e39
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-06-08 18:08:43 -0400

    Added ability to select semester and fixed the way the initial html is selected.

--- main.py
+++ main.py
@@ -4,22 +4,46 @@ from playwright.async_api import Playwright, async_playwright
 from tabulate import tabulate
 
 
+async def select_semester(options):
+    semester_dict = {}
+    print("Available semesters:")
+
+    for i, option in enumerate(options):
+        value = await option.get_attribute('value')
+        semester = await option.inner_text()
+        semester_dict[i] = (value, semester)
+        print(f"Enter {i} for {semester}")
+
+    # Get user input
+    semester_index = int(input("Select a semester: "))
+    while semester_index not in semester_dict:
+        semester_index = int(input("Invalid number inputted, please select a valid number between 0-3: "))
+
+    selected_semester_value = semester_dict[semester_index][0]
+    return selected_semester_value
+
+
 async def run(playwright: Playwright, *, url: str, course: str) -> str:
     # Basic navigation to desired page.
-    browser = await playwright.chromium.launch(headless=True)
+    browser = await playwright.chromium.launch(headless=False)
     page = await browser.new_page()
     await page.goto(url)
 
     # Inputting desired class into testudo
     # course input: course-id-input
     await page.fill('#course-id-input', course)
+    select_element = page.locator("#term-id-input")
+    option_elements = await select_element.locator('option').all()
+    # Run method to display all semester & select one.
+    option_value = await select_semester(option_elements)
+    await select_element.select_option(value=option_value)
+
     await page.press('#course-id-input', 'Enter')
 
     # Get the class's html information
     try:
-        html = page.locator('xpath=/html/body/div[2]/div/div[2]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[2]')
-        div_html = await html.inner_html()
-        return div_html
+        html = await page.inner_html('body')
+        return html
     except Exception as e:
         print(f"Error: {e}")
         return "Not found."
@@ -36,7 +60,7 @@ def get_class_info(html):
     course_text = html.find_all('div', class_='approved-course-text')
 
     prereqs = course_text[0].text
-    prereqs = insert_newlines(prereqs, 75)
+    prereqs = insert_newlines(prereqs, 75)  # Remove "Prerequisite: " from start
 
     description = course_text[1].text
     description = insert_newlines(description, 75)
@@ -104,7 +128,7 @@ def display_class_info(info, class_id):
 
     with open(f"{class_id}_info.txt", 'w') as f:
         f.write("Class Details\n")
-        f.write(tabulate(class_details, headers=["Field", "Value"], tablefmt="grid"))
+        f.write(tabulate(class_details, tablefmt="grid"))
         f.write("\n\nSection Details\n")
         f.write(tabulate(section_details, headers="firstrow", tablefmt="grid"))
 
@@ -112,9 +136,11 @@ def display_class_info(info, class_id):
 async def main() -> None:
     # Use async_playwright context manager to close the Playwright instance
     # automatically.
+    url = 'https://app.testudo.umd.edu/soc/'
     class_id = input('Enter a class: ')
+
     async with async_playwright() as playwright:
-        html = await run(playwright, url='https://app.testudo.umd.edu/soc/', course=class_id)
+        html = await run(playwright, url=url, course=class_id)
         class_html = BeautifulSoup(html, 'html.parser')
 
     class_info = get_class_info(class_html)
